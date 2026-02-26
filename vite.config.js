2689d8c7ee748ba7c68546220991c6617a0e0939
Dzianis Minzakirau <denis@fideum.group>
2026-02-26 20:11:30 +0300

    form

---
+++ vite.config.js
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: 'src/registration.js',
+      output: {
+        entryFileNames: 'registration.bundle.js',
+        format: 'iife',
+        name: 'RegistrationBundle',
+      },
+    },
+    outDir: 'dist',
+  },
+})
